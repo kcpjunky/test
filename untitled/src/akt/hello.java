ecdfa8e9daf907bf4d7cdb03058798abee066c79
yasutaka umebayashi <umebayashi_yasutaka@cyberagent.co.jp>
2014-04-09 21:29:22 +0900

    added akutsu

---
+++ untitled/src/akt/hello.java
@@ -0,0 +1,10 @@
+package akt;
+
+/**
+ * Created by umebayashi yasutaka on 2014/04/09.
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.print("tes");
+    }
+}
