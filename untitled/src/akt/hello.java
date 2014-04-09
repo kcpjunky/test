736d836e059fd49bb2bb250f3603143fe52ae899
akutsu_keita <akutsu_keita@cyberagent.co.jp>
2014-04-09 22:11:27 +0900

    aktchange

--- untitled/src/akt/hello.java
+++ untitled/src/akt/hello.java
@@ -4,7 +4,7 @@ package akt;
  * Created by umebayashi yasutaka on 2014/04/09.
  */
 public class hello {
-    public static void main(String[] args) {
-        System.out.print("tes");
+    public static void main(String[] args){
+        System.out.print("testkanryo");
     }
 }
