3d20ff79721669a46ada8be463107035ed31c43c
yasutaka umebayashi <umebayashi_yasutaka@cyberagent.co.jp>
2014-04-17 22:06:36 +0900

    akutsu

---
+++ untitled/src/akutsu/FizzBuzz.java
@@ -0,0 +1,16 @@
+package akutsu;
+public class FizzBuzz {
+	public void fizzbuzz() {
+		for (int i = 1 ; i<= 100; i++) {
+			if (i % 15 == 0 ) {
+				System.out.println("fizzbuzz");
+			} else if (i % 5 == 0 ) {
+				System.out.println("buzz");
+			} else if (i % 3 == 0 ) {
+				System.out.println("fizz");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+}
---
+++ untitled/src/akutsu/hello.java
@@ -0,0 +1,10 @@
+package akutsu;
+
+/**
+ * Created by umebayashi yasutaka on 2014/04/09.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.print("testkanryo2");
+    }
+}
